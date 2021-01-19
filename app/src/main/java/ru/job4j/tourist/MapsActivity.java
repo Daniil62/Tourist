681f2ce403097a8f192ebe1d60e576eca5a00859
yor_email <daniil.stebnitsky@gmail.com>
2021-01-19 18:51:42 +0300

    2. Установка маркера в Maps. [188118#260330]

---
+++ app/src/main/java/ru/job4j/tourist/MapsActivity.java
@@ -0,0 +1,30 @@
+package ru.job4j.tourist;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng park = new LatLng(54.610270, 39.728236);
+        googleMap.addMarker(new MarkerOptions().position(park).title("Central Park of Culture and Rest"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(park, 15));
+    }
+}
